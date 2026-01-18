38ad4324f9bc567c4557343551d53303375f3909
arnold-O <arnoldoghiator@gmail.com>
2026-01-18 23:56:32 +0100

    multiple variable syntax

--- python-datatypes/variables.py
+++ python-datatypes/variables.py
@@ -7,4 +7,11 @@ price = 15
 #The above shows that we can save these items in variables
 # In python declaration and initialization are done together
 # The equal sign you see is called assignment "="
+# unlike other languages the declaration of the datatypes can be ommited i python
+# Declaring multiple variables
 
+x,y,z = 1,2,"Play"
+# what this means x=1; y=2; z="Play"
+
+# If variable have same value
+m=o=p="Sensational"
