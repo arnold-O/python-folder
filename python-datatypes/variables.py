b7bbdd2eb511492daeb293f9d23d610dd2f93646
arnold-O <arnoldoghiator@gmail.com>
2026-01-18 23:46:10 +0100

    Intro to variables

---
+++ python-datatypes/variables.py
@@ -0,0 +1,10 @@
+# Variables are the names give to data or the reference to a data.
+
+#examples of variables
+name_of_product = "soap"
+price = 15
+
+#The above shows that we can save these items in variables
+# In python declaration and initialization are done together
+# The equal sign you see is called assignment "="
+
