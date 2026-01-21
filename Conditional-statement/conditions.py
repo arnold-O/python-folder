adce31f394efb9e6a60f030e1343ad865d27f1a9
arnold-O <arnoldoghiator@gmail.com>
2026-01-21 22:09:57 +0100

    intro on if statement and also introducing the operators

---
+++ Conditional-statement/conditions.py
@@ -0,0 +1,23 @@
+# Normally statements in python execute one after the other but conditional statement helps with the flow control ,
+# for them not to execute just one line after another but for each lin to execute due to a certain reason/criteria me
+
+
+#operators used are either relational or logical
+
+# < less than
+# > greater thn
+# <= less than or equal to
+# >= greater than or equal to
+# =! not equal to
+# == equal to
+
+#logical operators
+# or = OR
+# and = AND
+# not = NOT
+
+
+#the above are the relational and logical operators
+
+
+
