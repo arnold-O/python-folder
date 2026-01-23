fc4c597ab416afb268e07a0ff8a354a66819a6f6
arnold-O <arnoldoghiator@gmail.com>
2026-01-23 23:44:27 +0100

    control flow

---
+++ control-flow/control.py
@@ -0,0 +1,18 @@
+#for loop (iterate over something)
+for i in range(5):
+    print(i)
+#    Common uses:
+
+    for char in "hello":
+        print(char)
+
+    for item in [1, 2, 3]:
+        print(item)
+
+
+#while loop (repeat while condition is true)
+count = 0
+
+while count < 5:
+    print(count)
+    count += 1
