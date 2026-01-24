d70de9da5297f980db8983996f46b74c547158ff
arnold-O <arnoldoghiator@gmail.com>
2026-01-24 15:07:35 +0100

    exercise on palindrone and numbers

--- control-flow/control.py
+++ control-flow/control.py
@@ -13,20 +13,80 @@
 #    print(count + 1, "Hello from the main page")
 #    count += 1
 
-count  = int(input("Enter a number "))
+#count  = int(input("Enter a number "))
 
 
-while count > 0:
-    r = count % 10
-    count = count // 10
-    print(r)
+#while count > 0:
+#    r = count % 10
+#    count = count // 10
+#    print(r)
+
+
+#Display a multiplication table for any given number
+
+#no = int(input("Enter a number "))
+#multiply = 1
+
+#while  multiply <= 10:
+#    r = no * multiply
+#    print(no, "X", multiply, "=", r)
+#    multiply = multiply + 1
+
+
+######*************** STUDENT CHALLENGE************************
+
+# i. counting the number of Digit in a number
+
+#num = int(input("Enter a number "))
+#count = 0
+#while num > 0 :
+#    num = num // 10
+#    count += 1
+#print("The number has", count, "digit")
+
+# ii. find the sum of Digit in a number
+
+#num = int(input("Enter a number "))
+#original = 0
+
+#while num > 0:
+#    r = num % 10
+#    num = num // 10
+#    original = original + r
+#print(original)
+
+# iii. Reversing a number
+
+
+#num = int(input("Enter a number "))
+#original = 0
+
+#while num > 0:
+#    r = num % 10
+#    num = num // 10
+#   original = original * 10 + r
+#print(original)
 
 
 
+# iii. check if a number is palindrome
+
+num = int(input("Enter a number "))
+b = num
+original = 0
+
+while num > 0:
+    r = num % 10
+    num = num // 10
+    original = original * 10 + r
+
+print(original)
+if original == b:
+    print("This is a palindrome")
+else:
+    print("This is not a palindrome")
 
 #for loop (iterate over something)
-
-
 
 #for i in range(5):
 #    print(i)
