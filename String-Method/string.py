21b86d6b28d0ab886c366d26aff310d4addd5622
arnold-O <arnoldoghiator@gmail.com>
2026-01-30 23:52:18 +0100

    initializing formatting directory

--- String-Method/string.py
+++ String-Method/string.py
@@ -222,14 +222,14 @@
 #Anagram
 
 
-s1 = input("Enter a string ")
-s2 = input("Enter a second String ")
-
-if len(s1) != len(s2):
-    print("Not  Anagram")
-else:
-    for x in s1:
-        if x not in s2:
-            print("not Anagram")
-    else:
-        print("Anagram")
+# s1 = input("Enter a string ")
+# s2 = input("Enter a second String ")
+#
+# if len(s1) != len(s2):
+#     print("Not  Anagram")
+# else:
+#     for x in s1:
+#         if x not in s2:
+#             print("not Anagram")
+#     else:
+#         print("Anagram")
