b4f01aa24b5b67f9e72ee8c47f5962e0903f42c6
arnold-O <arnoldoghiator@gmail.com>
2026-02-08 23:47:16 +0100

    Looping Nested lis

--- List-method/list-python.py
+++ List-method/list-python.py
@@ -177,3 +177,21 @@ students = [
 
 print(students[1][0])  # Ben
 print(students[2][1])  # 14
+
+#Using for loops to transverse nested list
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    for number in row:
+        print(number)
+
+#With INdexes and range
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[i])):
+        print(matrix[i][j])
